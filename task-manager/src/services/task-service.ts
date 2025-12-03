46aa24adc59696fb73c50520fcf5f78df221aefa
Simren Chandan <simrenkaursoor@gmail.com>
2025-12-03 14:11:04 +0000

    feat(task-manager): created a service
    This service provides methods for CRUD operations,
    saves and stores tasks to local storage, and exposes signals for task changes.

---
+++ task-manager/src/services/task-service.ts
@@ -0,0 +1,45 @@
+import {Injectable, signal} from '@angular/core';
+import {Task} from '../models/task-model';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class TaskService {
+  private readonly localStorageKey = 'task-items';
+  private readonly _tasks = signal<Task[]>(this.getTasks());
+  public readonly tasks= this._tasks.asReadonly();
+
+  private getTasks(): Task[] {
+    const tasks = localStorage.getItem(this.localStorageKey);
+    return tasks ? JSON.parse(tasks) : [];
+  }
+
+  private saveTasks(tasks: Task[]): void {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(tasks));
+  }
+
+  public addTask(task: Task): void {
+    const currentTasks = [...this._tasks(), task];
+    this._tasks.set(currentTasks);
+    this.saveTasks(currentTasks);
+  }
+
+  public updateTask(updatedTask: Task): void {
+    const currentTaskUpdated = this._tasks().map(task => task.id === updatedTask.id ? updatedTask : task);
+    this._tasks.set(currentTaskUpdated);
+    this.saveTasks(currentTaskUpdated);
+  }
+
+  public deleteTask(id: number): void {
+    const deletedTaskUpdated = this._tasks().filter(task => task.id !== id);
+    this._tasks.set(deletedTaskUpdated);
+    this.saveTasks(deletedTaskUpdated);
+  }
+
+  public getTaskById(id: number): Task | undefined {
+    return this._tasks().find(task => task.id === id);
+  }
+
+}
